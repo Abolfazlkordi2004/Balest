c11f06b568fb06c9065aaddf83772e48a5f06a2b
myprojects <aboolfazlkordi83@gmail.com>
2025-07-07 21:24:54 +0330

    step 5

--- src/app/components/imageSlider.tsx
+++ src/app/components/imageSlider.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import SlideContent from "./slideContent";
 import { AnimatePresence } from "framer-motion";
+import SlideContent from "./slideContent";
 
 interface Slide {
   src: string;
@@ -24,7 +24,7 @@ export default function ImageSlider() {
   useEffect(() => {
     const timer = setInterval(() => {
       setIndex((prev) => (prev + 1) % slides.length);
-    }, 7000);
+    }, 5000);
     return () => clearInterval(timer);
   }, [slides]);
 
@@ -32,11 +32,52 @@ export default function ImageSlider() {
 
   const current = slides[index];
 
+  const handlePrev = () => {
+    setIndex((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
+  const handleNext = () => {
+    setIndex((prev) => (prev + 1) % slides.length);
+  };
+
   return (
     <div className="w-full h-[900px] relative overflow-hidden">
       <AnimatePresence mode="wait">
         <SlideContent key={current.text} src={current.src} text={current.text} />
       </AnimatePresence>
+
+      {/* دکمه‌های چپ و راست */}
+      <div className="absolute bottom-0 right-20 z-20 flex items-center gap-1">
+        <button
+          onClick={handlePrev}
+          className="bg-gray-900 hover:bg-[#F9A220] text-gray-800 p-3 transition"
+        >
+          <svg
+            className="w-8 h-8 text-white"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>
+
+        <button
+          onClick={handleNext}
+          className="bg-gray-900 hover:bg-[#F9A220] text-gray-800 p-3 transition"
+        >
+          <svg
+            className="w-8 h-8 text-white"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
