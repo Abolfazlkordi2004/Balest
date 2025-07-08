effc033da5c6a57a0bac9dce7aa2080f8fefb952
myprojects <aboolfazlkordi83@gmail.com>
2025-07-09 01:01:58 +0330

    step 9

--- src/app/components/projectComp.tsx
+++ src/app/components/projectComp.tsx
@@ -11,36 +11,46 @@ type IProjectCompProps = {
 };
 
 function ProjectComp() {
-  const [data, SetData] = useState<IProjectCompProps>();
+  const [data, SetData] = useState<IProjectCompProps[]>();
+
   useEffect(() => {
     axios
       .get("http://localhost:3001/projectContent")
       .then((response) => SetData(response.data))
-      .then((error) => console.error(error));
-  });
+      .catch((error) => console.error(error));
+  }, []); 
+
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex flex-row-reverse items-center justify-center w-2/3 h-[500px] gap-10">
-        <div>
-          <Link href="#">
-            <Image
-              src={data!.img}
-              alt="project-image"
-              width={900}
-              height={800}
-            />
-          </Link>
+    <div className="flex flex-col justify-center items-center gap-10 py-10">
+      {data?.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col md:flex-row-reverse items-center w-2/3 gap-10 border-b pb-10"
+        >
+          <div>
+            <Link href="#">
+              <Image
+                src={item.img}
+                alt="project-image"
+                width={500}
+                height={400}
+                className="rounded-xl"
+              />
+            </Link>
+          </div>
+          <div dir="rtl" className="flex flex-col justify-center text-right">
+            <Link href="#">
+              <h1 className="text-3xl font-bold">{item.header}</h1>
+            </Link>
+            <p className="text-lg text-gray-700 my-5 leading-relaxed">
+              {item.text}
+            </p>
+            <Link href="#">
+              <p className="text-base text-[#F9A220] mt-4">بیشتر بخوانید</p>
+            </Link>
+          </div>
         </div>
-        <div dir="rtl" className="flex flex-col justify-center">
-          <Link href="#">
-            <h1 className="text-4xl">{data?.header}</h1>
-          </Link>
-          <p className="text-xl text-gray-700 my-5">{data?.text}</p>
-          <Link href="#">
-            <p className="text-l text-[#F9A220] mt-10">بیشتر بخوانید </p>
-          </Link>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
