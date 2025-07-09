6e2bbf579b885fbd437818373fcf777a51098a47
myprojects <aboolfazlkordi83@gmail.com>
2025-07-09 19:00:57 +0330

    step 11

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -96,6 +96,102 @@ function HomePage() {
         </div>
         <ProjectComp />
       </div>
+      <div className="w-full h-[600px] bg-[#F9A220] overflow-hidden relative px-60">
+        <Image
+          src="/img/pattern-2.png"
+          alt="pattern"
+          fill
+          className="object-contain object-right right-0 z-0 absolute"
+        />
+        <div className="flex justify-end w-full relative z-20 mt-15">
+          <h1 className="text-4xl font-bold text-right z-10">در تماس باشید</h1>
+          <hr className="w-10 border-t-3 border-black mx-3 mt-8" />
+        </div>
+        <div className="flex justify-center items-center w-full h-full pb-35">
+          <div className="flex flex-col w-1/2 h-full justify-center items-center z-10">
+            <div className="w-[450px] my-5 relative ">
+              <input
+                type="text"
+                placeholder="نام خود را وارد کنید "
+                className="w-full text-black text-right text-xl placeholder-black focus:outline-none"
+              />
+              <hr className="w-full text-black mt-5" />
+            </div>
+            <div className="w-[450px] my-5 relative ">
+              <input
+                type="email"
+                placeholder=" ایمیل خود را وارد کنید "
+                className="w-full text-black text-right text-xl placeholder-black focus:outline-none"
+              />
+              <hr className="w-full text-black mt-5" />
+            </div>
+            <div className="w-[450px] h-[100px] my-5 relative ">
+              <textarea
+                className="w-full h-full text-black text-right text-xl placeholder-black focus:outline-none"
+                placeholder="پیغام شما"
+              ></textarea>
+              <hr className="w-full text-black mt-5" />
+            </div>
+            <div className="w-[450px] mt-10 text-right">
+              <button className="text-l text-[#F9A220] bg-black opacity-80 w-[70px] h-[40px] cursor-pointer">
+                ثبت
+              </button>
+            </div>
+          </div>
+          <hr className="w-[500px] rotate-90 my-10" />
+          <div className="flex flex-col w-1/2 h-full justify-center items-center z-10">
+            <div className="flex flex-row-reverse w-full gap-10 ">
+              <div className="w-1/4">
+                <Image
+                  src="/icons/icons8-map-50.png"
+                  alt="map"
+                  width={35}
+                  height={35}
+                />
+              </div>
+              <div dir="rtl" className="w-3/4">
+                <h2 className="font-bold text-lg">دفترکار</h2>
+                <p className="text-lg">
+                  تهران، کارگر شمالی <br /> پایین‌تر از تقاطع جلال آل‌احمد، پلاک{" "}
+                  <br />
+                  ۱۲ کد پستی ۸۳۸۴۳ - ۱۴۱۳۶
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row-reverse w-full gap-10 my-10">
+              <div className="w-1/4">
+                <Image
+                  src="/icons/icons8-envelope-50.png"
+                  alt="map"
+                  width={35}
+                  height={35}
+                />
+              </div>
+              <div className="w-3/4" dir="rtl">
+                <h2 className="font-bold text-lg">ایمیل</h2>
+                <p className="text-lg">info@sazian.net</p>
+                <p className="text-lg">support@sazian.net</p>
+              </div>
+            </div>
+            <div className="flex flex-row-reverse w-full gap-10">
+              <div className="w-1/4">
+                <Image
+                  src="/icons/icons8-phone-50.png"
+                  alt="map"
+                  width={35}
+                  height={35}
+                />
+              </div>
+              <div className="w-3/4" dir="rtl">
+                <h2 className="font-bold text-lg">تماس</h2>
+                <p className="text-lg">۸۸۳۵۲۲۱۸</p>
+                <p className="text-lg">۸۸۳۵۲۲۳۴</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </>
   );
 }
